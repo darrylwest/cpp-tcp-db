dee24d029aefbb79241aa5b2fc5e292d6d45159d
darryl west <darryl.west@raincitysoftware.com>
2025-04-22 16:25:55 -0700

    banner and file logger

--- src/tcpdb.cpp
+++ src/tcpdb.cpp
@@ -5,18 +5,47 @@
 #include <spdlog/spdlog.h>
 #include <unistd.h>
 
+#include <tcpdb/base.hpp>
 #include <tcpdb/server.hpp>
 #include <tcpdb/version.hpp>
+#include <print>
+#include <termio/termio.hpp>
+#include "spdlog/sinks/rotating_file_sink.h"
+
+constexpr int LOG_SIZE = 1'000'000;
 
 // configure the server logs
+void configure_logs(const std::string& logfile, const bool rolling) {
+    if (rolling) {
+        auto logger = spdlog::rotating_logger_mt("tcpdb", logfile, LOG_SIZE, 3);
+        spdlog::set_default_logger(logger);
+    }
 
+    spdlog::set_level(spdlog::level::info);
+    spdlog::flush_on(spdlog::level::info);
+}
+
+const char* BANNER = R"(
+  ________________            ____
+ /_  __/ ____/ __ \      ____/ / /_
+  / / / /   / /_/ /_____/ __  / __ \
+ / / / /___/ ____/_____/ /_/ / /_/ /
+/_/  \____/_/          \__,_/_.___/
+)";
 
 int main() {
     const auto pid = getpid();
     const auto vers = tcpdb::Version();
+
+    using namespace termio::termio;
+    std::println("{}{}{}", green(), BANNER, reset());
+    std::println("{}Version: {}, {}", cyan(), vers.to_string(), reset());
+    std::println("{}pid: {}{}", yellow(), pid, reset());
+
     tcpdb::config::Config config;
 
-    // TODO configure logging and data
+    std::println("{}logs: {}{}", green(), config.server.logs, reset());
+    configure_logs(config.server.logs, true);
 
     spdlog::info("tcp-db server, version: {}, pid: {}", vers.to_string(), pid);
 
