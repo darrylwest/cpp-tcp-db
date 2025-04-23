2855f7989dd1d38a4b92476e994f5a6e1cbb1a2c
darryl west <darryl.west@raincitysoftware.com>
2025-04-23 09:07:06 -0700

    version build bump

--- include/tcpdb/version.hpp
+++ include/tcpdb/version.hpp
@@ -16,7 +16,7 @@ namespace tcpdb {
         int major = 0;
         int minor = 3;
         int patch = 8;
-        int build = 136;
+        int build = 137;
 
         friend std::ostream& operator<<(std::ostream& os, const Version v) {
             // better to use <format> but it breaks on linux and fmt broken on darwin
